1a6ef05b836ac33b345da3df275b6de2c4af9f0a
saurabhdaware <saurabhdaware99@gmail.com>
2021-07-07 22:40:10 +0530

    add colors for find and match, selection, improve breadcrump colors

--- example-files/test.js
+++ example-files/test.js
@@ -1,9 +1,29 @@
-console.log(prop.xyz);
-function sxyz() {
-  if (x === 3) {
-    
+const red = (message) => `\u001b[31m${message}\u001b[39m`;
+const cyan = (message) => `\u001b[36m${message}\u001b[39m`;
+const boldCyan = (message) => `\u001b[1m\u001b[36m${message}\u001b[39m\u001b[22m`;
+const green = (message) => `\u001b[32m${message}\u001b[39m`;    
+const boldGreen = (message) => `\u001b[1m\u001b[32m${message}\u001b[39m\u001b[22m`;
+const boldRed = (message) => `\u001b[1m\u001b[31m${message}\u001b[39m\u001b[22m`;
+const yellow = (message) => `\u001b[33m${message}\u001b[39m`;
+const boldYellow = (message) => `\u001b[1m\u001b[33m${message}\u001b[39m\u001b[22m`;
+const grey = (message) => `\u001b[90m${message}\u001b[39m`;
+const boldGrey = (message) => `\u001b[1m\u001b[90m${message}\u001b[39m\u001b[22m`;
+const bold = (message) => `\u001b[1m${message}\u001b[22m`;
+const blue = (message) => `\u001b[34m${message}\u001b[39m`;
+const boldBlue = (message) => `\u001b[1m\u001b[34m${message}\u001b[39m\u001b[22m`;
 
-  }
-
-  return xyz;
-}
+// TERMINAL COLORS TEST
+console.log(boldGreen('green'));
+console.log(boldRed('red'));
+console.log(boldYellow('yellow'));
+console.log(boldGrey('grey'));
+console.log(boldCyan('cyan'));
+console.log(boldBlue('blue'));
+console.log(bold('bold'));
+console.log(grey('grey'));
+console.log(blue('blue'));
+console.log(yellow('yellow'));
+console.log(green('green'));
+console.log(red('red'));
+console.log(cyan('cyan'));
+console.log('normal');
